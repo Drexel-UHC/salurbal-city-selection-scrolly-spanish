cc353c81895bef13a0276bd446959b452a96497e
rl627 <rl627@drexel.edu>
2023-09-26 15:57:20 -0400

    added spanish version GA tag + disabled superflow for prod

--- src/App.svelte
+++ src/App.svelte
@@ -32,10 +32,10 @@
   import Toggle from './ui/Toggle.svelte';
   import Arrow from './ui/Arrow.svelte';
 
-  import { initSuperflow } from '@usesuperflow/client';
-  initSuperflow('t3p1NHY1MaAX795EXt7m', {
-    projectId: '501229561750262',
-  });
+  // import { initSuperflow } from '@usesuperflow/client';
+  // initSuperflow('t3p1NHY1MaAX795EXt7m', {
+  //   projectId: '501229561750262',
+  // });
 
   let theme = 'light';
   setContext('theme', theme);
